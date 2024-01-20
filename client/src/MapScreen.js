93f77b4f6b372a6e311148db0d14215c149e98eb
jc-c1 <joannec.chang1@gmail.com>
2024-01-20 15:21:24 -0800

    added Google Map API to MapScreen

--- client/src/MapScreen.js
+++ client/src/MapScreen.js
@@ -1,11 +1,26 @@
-// src/OtherScreen.js
+// src/MapScreen.js
 import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const MapScreen = ({ setScreen }) => {
+  const mapContainerStyle = {
+    width: '100%',
+    height: '400px',
+  };
+
+  const center = {
+    lat: 37.7749, // Latitude for San Francisco, you can adjust this to your preferred location
+    lng: -122.4194, // Longitude for San Francisco, you can adjust this to your preferred location
+  };
+
   return (
     <div>
       <h2>Map</h2>
-      <p>This is Map</p>
+      <LoadScript googleMapsApiKey="">
+        <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={12}>
+          {/* Additional components or markers can be added here */}
+        </GoogleMap>
+      </LoadScript>
       <button onClick={() => setScreen('camera')}>Go to Camera Upload</button>
     </div>
   );
