35e34f79e177fc8da7d91e6385f2c70169bbd0ca
jc-c1 <joannec.chang1@gmail.com>
2024-01-20 22:14:21 -0800

    added css to nav bar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -3,21 +3,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // Import the CSS file
 
+// const Navbar = () => {
+//     return (
+//         <nav className="navbar">
+//             <div className="menu-icon">&#9776;
+//             </div>
+//             <ul className="nav-links">
+//                 <li>
+//                     <Link to="/">Home</Link>
+//                 </li>
+//                 <li>
+//                     <Link to="/upload">Camera Upload</Link>
+//                 </li>
+//                 {/* Add more navigation links as needed */}
+//             </ul>
+//         </nav>
+//     );
+// };
+
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className="menu-icon">&#9776;</div>
-            <ul className="nav-links">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/camera">Camera Upload</Link>
-                </li>
-                {/* Add more navigation links as needed */}
-            </ul>
-        </nav>
+      <nav className="navbar">
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/upload">Camera Upload</Link>
+          </li>
+          <li>
+            <Link to="/image-details">View Image Details</Link>
+          </li>
+          {/* Add more navigation links as needed */}
+        </ul>
+      </nav>
     );
-};
+  };
 
 export default Navbar;
