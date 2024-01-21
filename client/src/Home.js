27f30cb2fb83df5d0c9533c787f18a048570a7d8
jc-c1 <joannec.chang1@gmail.com>
2024-01-21 11:58:35 -0800

    added logo

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,8 +3,13 @@ import './Home.css';
 
 const Home = () => {
     return (
+        <div>
+            <div className="logo">
+            <img src={require('./Home icon/centered logo.png')} className="logoimg" />
+        </div>
         <div className="container">
             <img src={require('./Home icon/Main Page.jpeg')} className="TS" />
+        </div>
         </div>
     );
 };
