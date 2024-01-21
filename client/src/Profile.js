bce7822d03b7425a97174d945d2c1d81d1344bc9
Sigma <sigma@Sigmas-MacBook-Air.local>
2024-01-21 02:59:44 -0800

    profile finish

---
+++ client/src/Profile.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './Profile.css';
+
+const Profile = () => {
+
+    return (
+        <div className="profile-container">
+            <img src={require('./UI icon/Scroll_group.png')} className="TS" />
+        </div>
+    );
+};
+
+export default Profile;
+
